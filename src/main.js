60199de7933b51a14686ac2dd9a1b107a1283c3c
Nick Hennessy <njhnny@gmail.com>
2020-11-12 15:05:10 -0800

    accept date from user
    Co-authored-by: Joe Nero <josephnero111@gmail.com>
    Co-authored-by: Callie Stump <callie@stu.mp>

--- src/main.js
+++ src/main.js
@@ -6,5 +6,11 @@ import './css/styles.css';
 $(document).ready(function() {
   $("form#calculator").submit(function(event) {
     event.preventDefault();
+    let input = $('#date').val();
+    console.log(input);
+    let dateInput = new Date(input);
+    let weekday = dateInput.getDay();
+    console.log(weekday);
+    $("p").text();
   });
 });
